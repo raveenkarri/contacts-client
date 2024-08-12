f53f7595cc8399e2be2574c4a53f0bd92b615381
raveenkarri <raveenkarri@gmail.com>
2024-08-12 22:29:23 +0530

    bug fix

--- src/index.js
+++ src/index.js
@@ -4,25 +4,26 @@ import "./index.css";
 import App from "./App";
 import axios from "axios";
 import Cookies from "js-cookie";
-
 import { BrowserRouter } from "react-router-dom";
 
 // Set default configurations for axios
-
 axios.defaults.baseURL = "https://contacts-api-dhfb.onrender.com/api/users";
 
+// Create a context for state management
 export const stateContext = createContext();
 
 // StateProvider component
 export const StateProvider = ({ children }) => {
-  const [token, setToken] = useState("");
+  const [token, setToken] = useState(() => Cookies.get("token") || "");
+
   useEffect(() => {
     if (token) {
-      Cookies.set("token", token);
+      Cookies.set("token", token, { expires: 7 });
     } else {
       Cookies.remove("token");
     }
   }, [token]);
+
   return (
     <stateContext.Provider value={{ token, setToken }}>
       {children}
